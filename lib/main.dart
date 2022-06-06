448678757ab38e9b987e81ead302c1e67110d571
junxkanaki <pjsouognl@gmail.com>
2022-06-06 16:30:54 +0900

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,27 @@
 import 'package:flutter/material.dart';
+import 'stateful_widget_pattern/pages/home_page.dart' as stateful_widget_pattern;
+import 'provider_pattern/pages/home_page.dart' as provider_pattern;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}):super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'State Management',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const stateful_widget_pattern.TopPage(),
+        '/statefulWidgetPattern': (context) => stateful_widget_pattern.TopPage(),
+        // '/providerPattern':(context) => const provider_pattern.TopPage(),
+      },
     );
   }
 }
